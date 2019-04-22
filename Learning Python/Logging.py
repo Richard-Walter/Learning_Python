fa2e59914e5c3ce79f56e7da829038391cc94c0d
Richard Walter <willthisnamedo@gmail.com>
2019-04-22 18:06:14 +1000

    Added advanced logging tutorial

--- Learning Python/Logging.py
+++ Learning Python/Logging.py
@@ -11,9 +11,63 @@ import logging
 
 
 # Default logging level is WARNING and log to console.  Change logging configuration
-# Different format codes are available-see docs for various options
-logging.basicConfig(filename='test.log', level=logging.DEBUG,
-                    format='%(asctime)s:%(levelname)s:%(message)s')
+# Different format codes are available-see python logging docs for various options
+# logging.basicConfig(filename='test.log', level=logging.DEBUG,
+#                     format='%(asctime)s:%(levelname)s:%(message)s')
+#
+#
+# def add(x, y):
+#     """Add Function"""
+#     return x + y
+#
+#
+# def subtract(x, y):
+#     """Subtract Function"""
+#     return x - y
+#
+#
+# def multiply(x, y):
+#     """Multiply Function"""
+#     return x * y
+#
+#
+# def divide(x, y):
+#     """Divide Function"""
+#     return x / y
+#
+#
+# num_1 = 10
+# num_2 = 5
+#
+# add_result = add(num_1, num_2)
+# logging.debug('Add: {} + {} = {}'.format(num_1, num_2, add_result))
+#
+# sub_result = subtract(num_1, num_2)
+# logging.debug('Sub: {} - {} = {}'.format(num_1, num_2, sub_result))
+#
+# mul_result = multiply(num_1, num_2)
+# logging.debug('Mul: {} * {} = {}'.format(num_1, num_2, mul_result))
+#
+# div_result = divide(num_1, num_2)
+# logging.debug('Div: {} / {} = {}'.format(num_1, num_2, div_result))
+
+
+"""ADVANCED LOGGING-handlers, formatter, exceptions"""
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+
+file_handler = logging.FileHandler('sample.log')
+file_handler.setLevel(logging.ERROR)
+file_handler.setFormatter(formatter)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 
 
 def add(x, y):
@@ -33,20 +87,25 @@ def multiply(x, y):
 
 def divide(x, y):
     """Divide Function"""
-    return x / y
+    try:
+        result = x / y
+    except ZeroDivisionError:
+        logger.exception('Tried to divide by zero')
+    else:
+        return result
 
 
 num_1 = 10
-num_2 = 5
+num_2 = 0
 
 add_result = add(num_1, num_2)
-logging.debug('Add: {} + {} = {}'.format(num_1, num_2, add_result))
+logger.debug('Add: {} + {} = {}'.format(num_1, num_2, add_result))
 
 sub_result = subtract(num_1, num_2)
-logging.debug('Sub: {} - {} = {}'.format(num_1, num_2, sub_result))
+logger.debug('Sub: {} - {} = {}'.format(num_1, num_2, sub_result))
 
 mul_result = multiply(num_1, num_2)
-logging.debug('Mul: {} * {} = {}'.format(num_1, num_2, mul_result))
+logger.debug('Mul: {} * {} = {}'.format(num_1, num_2, mul_result))
 
 div_result = divide(num_1, num_2)
-logging.debug('Div: {} / {} = {}'.format(num_1, num_2, div_result))
+logger.debug('Div: {} / {} = {}'.format(num_1, num_2, div_result))
